b59112a39927b0c40e5314c122c03068db3b5b9f
Jonathan Abbott <jonathan.abbott@hippodigital.co.uk>
2026-06-25 08:33:02 +0100

    buigfix: fix location assert on global resouces / resrouces with no location

--- tests/elz/DFE_ELZ_0002.tf
+++ tests/elz/DFE_ELZ_0002.tf
@@ -4,6 +4,13 @@ resource "azurerm_resource_group" "pass" {
   tags     = local.common_tags
 }
 
+resource "azurerm_management_lock" "pass_no_location" {
+  name       = "test-lock"
+  scope      = azurerm_resource_group.pass.id
+  lock_level = "CanNotDelete"
+  tags       = local.common_tags
+}
+
 resource "azurerm_resource_group" "fail" {
   name     = "${local.prefix}rg-project-name"
   location = "westeurope"
